da075a6f38667a0922800741a318e2abb1be5417
André Labuschagné <andre.labuschagne@dariel.co.za>
2018-01-30 15:13:00 +0200

    Last refactoring

--- includes/config.php
+++ includes/config.php
@@ -20,9 +20,12 @@ $loginAttemptCount = 3;
 $loginAttemptTime  = strtotime( '-2 minutes' );
 
 //Database connect (Most of the time these can be kept the same)
-//This is for php 5 - comment out or remove if using php 7
-//$dbCon = mysql_pconnect( $dbHost , $dbUsername , $dbPassword ) or die( mysql_error() );
-//mysql_select_db( $dbDatabase , $dbCon );
-//This is for php 7 - uncomment if using php 7
-$dbCon = mysqli_connect( $dbHost , $dbUsername , $dbPassword ) or die( mysql_error() );
-mysqli_select_db( $dbCon, $dbDatabase );
+if(!isVer7()) {
+  //This is for php 5 - comment out or remove if using php 7
+  $dbCon = mysql_pconnect( $dbHost , $dbUsername , $dbPassword ) or die( mysql_error() );
+  mysql_select_db( $dbDatabase , $dbCon );
+} else {
+  //This is for php 7 - uncomment if using php 7
+  $dbCon = mysqli_connect( $dbHost , $dbUsername , $dbPassword ) or die( mysql_error() );
+  mysqli_select_db( $dbCon, $dbDatabase );
+}
